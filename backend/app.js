e477aabe4ab109d6357333d8ecd60a719a8764f8
Navneet Deshta <navneetdeshtaa@gmail.com>
2025-01-08 17:57:03 +0530

    update

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var cors = require("cors");
-
+var cors = require('cors');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const connectDB = require('./config/db');
@@ -15,12 +14,11 @@ var app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-const allowedOrigins = [
-  'http://localhost:5173', 
-  'https://dev-dock-ide-murex.vercel.app'
-];
+// CORS setup: Allow different origins based on environment
+const allowedOrigins = process.env.NODE_ENV === 'production'
+  ? ['https://dev-dock-ide-murex.vercel.app']
+  : ['http://localhost:5173', 'https://dev-dock-ide-murex.vercel.app'];
 
 app.use(cors({
   origin: function (origin, callback) {
@@ -29,19 +27,32 @@ app.use(cors({
     } else {
       callback(new Error('Not allowed by CORS'));
     }
-  }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
 }));
 
-// Routes
+// Static file serving
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build'))); // Serve React app in production
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html')); // Fallback to React's index.html
+  });
+}
 
-
+// API routes
 app.use('/api', indexRouter);
 app.use('/users', usersRouter);
-app.get('/', (req,res) => res.send("API Working"))
 
+// Root route
+app.get('/', (req, res) => res.send('API Working'));
 
 // Error handler
 app.use(function (err, req, res, next) {
+  if (process.env.NODE_ENV === 'development') {
+    console.error(err.stack);
+  }
   res.status(err.status || 500).json({ message: err.message });
 });
 
