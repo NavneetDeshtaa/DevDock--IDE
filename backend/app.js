f1cacd29b21c7784f855e18360c421729023519a
Navneet Deshta <navneetdeshtaa@gmail.com>
2025-01-08 17:40:47 +0530

    update

--- backend/app.js
+++ backend/app.js
@@ -19,11 +19,6 @@ app.use(cookieParser());
 // Serve static files in production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'public')));
-
-  // Serve the index.html for all non-API routes
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-  });
 }
 
 
