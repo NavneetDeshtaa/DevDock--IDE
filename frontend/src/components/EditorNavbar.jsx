da955edb9f6644024d3e39b9cb72e3f9e0044181
Navneet Deshta <navneetdeshtaa@gmail.com>
2025-07-02 15:12:45 +0530

    EDITOR-NAVBAR update

--- frontend/src/components/EditorNavbar.jsx
+++ frontend/src/components/EditorNavbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { FiDownload } from "react-icons/fi";
 import { IoIosArrowBack } from "react-icons/io";
 import { Code } from "lucide-react";
